2aa2bdd9a0672f644e4b33fd545cdfc9fdc6feff
stalin982 <antony.stanly@ccts.com>
2020-12-08 17:08:59 +0530

    updated deploy

--- DBtils.java
+++ DBtils.java
@@ -1,3 +1,7 @@
+001finally
+
 hello ...this file contains db logic..
 
 hello...tram ..Im from dev branch
+
+automatic update taking
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+hi..Iam updating on 8th december
+
 this file contains deployment instructions
 
 updated script
+
+automatic update
